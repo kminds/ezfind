ed987ad69c8bd1667de196e0e54fe3e084c4f196
Paul Borgermans <paul.borgermans@gmail.com>
2016-01-13 17:00:56 +0100

    Add option to override parameters with a project specific config file, adapted output messages slightly

--- solr/fetch-install-solr.sh
+++ solr/fetch-install-solr.sh
@@ -8,8 +8,14 @@ TARGET_ROOT_DIR='.'
 
 TARGET_DATA_DIR=${TARGET_ROOT_DIR}/server/solr
 
+# Override default config values if custom configuration file is present
+if [[ -f config.in.sh ]]
+then
+   . config.in.sh
+fi
 
-if [ -f $TEMP_ARCHIVE ];
+
+if [[ -f $TEMP_ARCHIVE ]];
 then
     echo "Using existing archive .."
 else
@@ -17,11 +23,13 @@ else
     curl -L $DOWNLOAD_URI_SOLR > $TEMP_ARCHIVE
 fi
 
-echo "Preparing target installation directory and extracting .. "
+echo ".. preparing target installation directory and extracting .. "
 mkdir -p $TARGET_ROOT_DIR
-tar -xzf /tmp/solr-archive.tgz -C $TARGET_ROOT_DIR --strip-components=1
+tar -xzf $TEMP_ARCHIVE -C $TARGET_ROOT_DIR --strip-components=1
 
-echo "Preparing data directories and installing default configsets .."
+echo ".. copying configsets (templates) to destination .."
 mkdir -p $TARGET_DATA_DIR
 
 cp -r configsets/* $TARGET_DATA_DIR/configsets
+
+echo "Done."
